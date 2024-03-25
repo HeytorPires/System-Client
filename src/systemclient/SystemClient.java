a7390f2c84d1d6096899a28e0fb9287fb1e430c0
HeytorPires <heytor.cacho29@gmail.com>
2024-03-25 20:13:53 -0300

    Arrumando Lógica de menu e listagem

--- src/systemclient/SystemClient.java
+++ src/systemclient/SystemClient.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  *
  * @author Fabiano
  */
-public class SystemClient {
+public class SystemClient  {
 
     /**
      * @param args the command line arguments
@@ -20,13 +20,8 @@ public class SystemClient {
         
         ArrayList<Cliente> clientes = new ArrayList<>();
         
-         clientes.add(cliente) ;
         Scanner Scan = new Scanner(System.in);
-        System.out.println("-----Menu-----");
-        System.out.println("1 - Cadastrar Cliente ");
-        System.out.println("2 - Listar Clientes");
-        System.out.println("3 - Sair!");
-        System.out.println("3 - Sair!");
+        Menu();
         int MenuInputUser = Scan.nextInt();
         //Cliente cliente1 = new Cliente("Juridico", "Pires", "documento Teste", 0);
         boolean Saida = false;
@@ -36,7 +31,7 @@ public class SystemClient {
                 System.out.println("---- Cadastro Cliente ----");
                 System.out.println("Digite tipo do cliente: 1- Fisico __ 2- Juridico:");
                 int TypeUserClient = Scan.nextInt();
-                String TypeCliente; 
+                String TypeCliente = "";
                 if(TypeUserClient == 1){
                  TypeCliente = "Fisico";
                 } else if( TypeUserClient == 2){
@@ -46,26 +41,24 @@ public class SystemClient {
                 String NameUserClient = Scan.next();
                 System.out.println("Documento:");
                 String DocumentUserClient = Scan.next();
-                System.out.println("-----Cliente Cadastrado com sucesso!----"); System.out.println("");
-                System.out.println("");
-                System.out.println("");
-                System.out.println("");
-                System.out.println("-----Menu-----");
-                System.out.println("1 - Cadastrar Cliente ");
-                System.out.println("2 - Listar Clientes");
+                System.out.println("-----Cliente Cadastrado com sucesso!----"); System.out.println(""); 
+                System.out.println("\n\n\n\n");
+                Menu();
+                System.out.println("\n\n\n\n");
                 MenuInputUser = Scan.nextInt();
                 Cliente cliente = new Cliente(TypeCliente, NameUserClient, DocumentUserClient, 0);
+                clientes.add(cliente);
+                
             }
             case 2 -> {
                  System.out.println(" ----- Listagem Cliente ----");
-                 System.out.println(clientes.get(0));
-                 System.out.println("");
-                System.out.println("");
-                System.out.println("");
-                System.out.println("");
-                System.out.println("-----Menu-----");
-                System.out.println("1 - Cadastrar Cliente ");
-                System.out.println("2 - Listar Clientes");
+                 for( int i = 0; i < clientes.size(); i++){
+                     System.out.println(clientes.get(i));
+                 };
+                 System.out.println("\n\n\n\n");
+                Menu();
+                
+
                 MenuInputUser = Scan.nextInt();
             
                     }
@@ -89,6 +82,12 @@ public class SystemClient {
         System.out.println("Documento do Cliente" + DocumentoCliente );
         */
 
+    }
+    public static void Menu(){
+        System.out.println("-----Menu-----");
+        System.out.println("1 - Cadastrar Cliente ");
+        System.out.println("2 - Listar Clientes");
+        System.out.println("3 - Sair!");
     }
     
 }
